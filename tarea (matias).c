e95fa7c76154683c5380b73018e3b77cd3897edd
matisin <matias.medina.s2@gmail.com>
2016-03-29 18:26:49 -0300

    tokens cualios

--- tarea (matias).c
+++ tarea (matias).c
@@ -7,24 +7,24 @@
 
 #define   buffer_n 100
 
-void parser(char s2[]){
-	char *s1 = malloc(250);
+char **parser(char s2[]){
 	char *copia;
-
-
+	char **tokens = malloc(buffer_n*sizeof(char));
 	printf("El string original es: %s\n", s2);
-
-
 	char delim[]=" \t";
 	char *token;
 	copia = strdup(s2);
 	token = strtok(s2,delim);
 	printf("%s\n", token);
-
+	*(tokens) =  token;
+	int i = 1;
 	while(token = strtok(NULL, delim))
 	{
 		printf("%s\n", token);
+		*(tokens+i) =  token;
+		i++;
 	}
+	return tokens;
 }
 
 // imprime el prompt
@@ -37,29 +37,44 @@ void printPrompt(){
 
 int main (int argc, char *argv[]) {
 
-	char *input;
+	char *input,**token;
 	input = (char *) malloc(buffer_n*sizeof(char));
+	token = (char **) malloc(buffer_n*sizeof(char));
+	int hay_pipe ; 
+	int i;
 
 	system("clear");
 
-
-
 	while(1) {
+		hay_pipe = 0;
 
-		printPrompt();//imprime el 
-
+		printPrompt();//imprime el prompt
 
 		fgets(input,buffer_n*sizeof(char),stdin);//leemos la entrada
-		parser(input); //aqui ingresamos el comando
-
 
 		if(*input == '\n') { //se compara el primer caracter primero
 			continue;
 		}
 
+		token=parser(input); //aqui ingresamos el comando
+		printf("%s\n",*(token) );
+
+		for(i = 0; i < buffer_n ; i++ ){
+			if(*(input+i)=='|'){//recoremos el input
+				hay_pipe = 1; //si es comando con pipe
+				break;
+			}
+		}
+		if(hay_pipe){
+			
+		}else{
+			
+		}
+
 	}
 
 	free(input);
+	free(token);
 
 	printf("Donoso culiao\n");
 }
