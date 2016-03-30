faccd318a9e690f888914177996e777c07f85efa
matisin <matias.medina.s2@gmail.com>
2016-03-30 15:50:14 -0300

    este archivo esta igual que el tarea.c

--- tarea (matias).c
+++ tarea (matias).c
@@ -1,51 +1,50 @@
 #include <stdio.h>
-
-
 #include <stdlib.h>
-
 #include <string.h>
 
 #define   buffer_n 1000
 
-
 // parsea un string de entrada
 //
 // return char **
-
-char **parser(char string[]){	
-	char **tokens = malloc(buffer_n*sizeof(char));	
+int parser(char *string,char **tokens){	
+	int hay_pipe = 0;
 	char *delim = " \t\n";
 	char *token =  strtok(string,delim);	
 	int i = 0;
 	while(token){			
 		*(tokens+i) =  token;
+		if(!hay_pipe){
+			if(*token=='|'){
+				hay_pipe=1;
+			}
+		}
 		token = strtok(NULL, delim);
 		i++;
 	}
-	return tokens;
+	return hay_pipe;
 }
 
 // imprime el prompt
 //
 // return void
-
 void printPrompt(){
 
 	printf("minishell$ ");
 }
+void busca_pipe(int *hay_pipe,char **tokens){
 
+}
 int main (int argc, char *argv[]) {
-
-	char *input,**token;
-	input = (char *) malloc(buffer_n*sizeof(char));
-	token = (char **) malloc(buffer_n*sizeof(char));
-	int hay_pipe ; 
+	
+	char *input = (char *) malloc(buffer_n*sizeof(char));
+	char **tokens = (char **) malloc(buffer_n*sizeof(char));
+	int hay_pipe; 
 	int i;
 
 	system("clear");
 
-	while(1) {
-		hay_pipe = 0;//se asume que no hay pipe al principio
+	while(1) {		
 
 		printPrompt();//imprime el prompt
 
@@ -53,35 +52,31 @@ int main (int argc, char *argv[]) {
 
 		if(*input == '\n') { //se compara el primer caracter primero
 			continue;
-		}
+		}			
 
-		token=parser(input); //se guardan los tokens
-
+		hay_pipe=parser(input,tokens); //se guardan los tokens 
+		
+		//Para imprimir tokens
 		for(i = 0 ;i < buffer_n*sizeof(char) ; i++){
 
-			if(*(token+i)==NULL){
+			if(*(tokens+i)==NULL){
 				break;
 			}
-			printf("%s \n",*(token+i));
-			
-		}
-
-		for(i = 0; i < buffer_n ; i++ ){
-			if(*(input+i)=='|'){//recoremos el input
-				hay_pipe = 1; //si es comando con pipe
-				break;
-			}
-		}
+			printf("%s \n",*(tokens+i));
+		}		
 		if(hay_pipe){
-			
+			printf("Hay pipe!\n");
 		}else{
-			
+			printf("No hay pipe!\n");
 		}
+		//reseteamos el input y los tokens.
+		memset(input,0,buffer_n*sizeof(char));
+		memset(tokens,0,buffer_n*sizeof(char));
 
 	}
 
 	free(input);
-	free(token);
+	free(tokens);
 
 	printf("Donoso culiao\n");
 }
